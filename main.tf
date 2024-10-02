15d4146241351913cf4623cd92a40ba0745ba1d4
divyavut <vdivyaaws@gmail.com>
2024-10-02 19:36:38 -0400

    vpc module

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "aws_db_subnet_group" "default" {
 
 resource "aws_route" "public" {
   route_table_id            = aws_route_table.public.id
-  destination_8cidr_block    = "0.0.0.0/0"
+  destination_cidr_block    = "0.0.0.0/0"
   gateway_id  = aws_internet_gateway.main.id
 }
 
